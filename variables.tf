994bde4543068939ee93d63687f230ae6763fac0
Yura <shupik3@gmail.com>
2023-07-14 18:18:46 +0300

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-  type    = string
-  default = "AKIA3JWEQ5JLXR5MPQNZ"
-}
-
-variable "aws_secret_key" {
-  type    = string
-  default = "6vcaUdNY/S7c+Q0Lho8xUhgEZt9kzbdKWyG6ZQd8"
-}
